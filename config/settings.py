f52f52d21f935a154e8c940f5f61dd69009dee7d
Nomadyb <Nomadaeyb@outlook.com>
2024-02-02 17:25:21 +0300

    Django: Project  migrate todo page and category  updated

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+#INSTALLED_APPS =
+
+BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +40,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS = [
+
+]
+
+MY_APPS = [
+    'todo',
+]
+
+
+
+INSTALLED_APPS = BASE_APPS + THIRD_PARTY_APPS + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- page/views.py
+++ page/views.py
@@ -24,35 +24,8 @@ def home_view(request):
     return render(request, "page/home_page.html", context)
 
 
-def about_us_view(request):
-    page_title = "Hakkımızda"
-    hero_content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut ornare risus. Aliquam ac eleifend dolor. Suspendisse consectetur, orci at laoreet efficitur, sem tellus scelerisque neque, quis fringilla dolor erat id leo. Curabitur non malesuada nibh. In eu ultricies elit. Quisque tincidunt convallis tellus sit amet eleifend. Nullam nec justo pellentesque, laoreet purus eget, accumsan nisi."
-    context = {
-        "page_title": page_title,
-    }
-    context['hero_content'] = hero_content
-    # context['FAKE_DB_PROJECTS'] = FAKE_DB_PROJECTS
-    return render(request, "page/about_us.html", context)
 
 
-def vision_view(request):
-    page_title = "Vizyonumuz"
-    context = dict(
-        page_title=page_title,
-        # FAKE_DB_PROJECTS=FAKE_DB_PROJECTS,
-    )
-    return render(request, "page/vision.html", context)
-
-
-def contact_us_view(request):
-    page_title = "İletişim"
-    hero_content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut ornare risus. Aliquam ac eleifend dolor. Suspendisse consectetur, orci at laoreet efficitur, sem tellus scelerisque neque, quis fringilla dolor erat id leo. Curabitur non malesuada nibh. In eu ultricies elit. Quisque tincidunt convallis tellus sit amet eleifend. Nullam nec justo pellentesque, laoreet purus eget, accumsan nisi."
-    context = dict(
-        page_title=page_title,
-        hero_content=hero_content,
-        # FAKE_DB_PROJECTS=FAKE_DB_PROJECTS,
-    )
-    return render(request, "page/contact_us.html", context)
 
 
 def page_view(request, slug):
@@ -67,3 +40,4 @@ def page_view(request, slug):
         # print(context)
         return render(request, "page/page_detail.html", context)
     raise Http404
+    
